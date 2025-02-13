8e731678bfe43d272f288218cb0014178e887264
pdms_ibs_brilliantPC <vjavmvja@gmail.com>
2025-02-13 16:57:30 +0900

    일단 함수들 이름 정함 ㅅㄱ

--- grid2d.m
+++ grid2d.m
@@ -1,53 +1,59 @@
 classdef grid2d
     %UNTITLED3 Summary of this class goes here
     %   Detailed explanation goes here
-
     properties
         offset_stdpts (1,2) double = [nan nan] %[o-p]_pC
         a1_stdpts (1,2) double = [nan nan] % [a_1]_pC
-        a2_stdpts (1,2) double = [nan nan] % [a_1]_pC
-        sz_stdpts (1,2) double = [nan nan] %
-        clr_a1 (1,3) double = [1,0,0]
-        clr_a2 (1,3) double = [0,1,0]
+        a2_stdpts (1,2) double = [nan nan] % [a_2]_pC
+        sz_stdpts (1,2) double = [nan nan] % [a_3]
+        clr_a1 = [1 0 0]
+        clr_a2 = [0 1 0]
+        clr_faceWindow = [0 1 1];
+        clr_edgeWindow = [0 0 1];
     end
     properties (Dependent)
         augMat (3,3) table % [I]_pC^oB
-        baryBss (2,3) table
+        baryBss (2,3) table        
     end
-
-    methods
+    properties (Dependent,Hidden)
+        rect (2,4) double        
+    end
+    
+    methods % about dependent properties
         function v = get.augMat(obj)
             v = [obj.a1_stdpts' obj.a2_stdpts' obj.offset_stdpts';0 0 1];
             v = array2table(v,"RowNames",{'x','y','aug'} ,"VariableNames",{'a1','a2','o'});
         end
-        function v = get.baryBss(obj)%barycentric basis
+        function v = get.baryBss(obj)%barycentric basis            
             v = [[0;0] obj.a1_stdpts' obj.a2_stdpts']-obj.offset_stdpts';
             v = array2table(v,"RowNames",{'x','y'} ,"VariableNames",{'p0(=o)','p1(=o+v1)','p2(=o+v2)'});
         end
+        function v = get.rect(obj)
+            sz = obj.sz_stdpts;
+            v = [0 sz(1) sz(1) 0;
+                0 0 sz(2) sz(2)];
+        end
+    end   
+    methods % calculate graphic object in Window
         function lattice_coords_in = fnd_Lattice_in_window(obj)
             I_oB__pC = inv(table2array(obj.augMat)) ;
-            sz = obj.sz_stdpts;
-            rect = [0 sz(1) sz(1) 0;
-                0 0 sz(2) sz(2)]
-            lattice_coords_in =  fnd_Lattice_in_window(I_oB__pC,rect)
+            lattice_coords_in =  fnd_Lattice_in_window(I_oB__pC,obj.rect)
         end
-
-
-
-        function visBss(obj)
-            hold on
-            for ibasis = 1:2
-                arr = obj.baryBss(:,[1 1+ibasis]);
-                plot(arr(1,:),arr(2,:))
-            end
-            hold off
+        function tbl_line = fnd_Grid_in_window(obj)
+            I_oB__pC = inv(table2array(obj.augMat)) ;
+            tbl_line = fnd_Grid_in_window(I_oB__pC,obj.rect);
         end
-        % function tbl = t_augMat(obj)
-        %     tbl = array2table(obj.augMat,"RowNames",{'x','y','aug'} ,"VariableNames",{'a1','a2','o'});
-        % end
-        % function tbl = t_baryBss(obj)
-        %     tbl = array2table(obj.baryBss,"RowNames",{'x','y'} ,"VariableNames",{'p0(=o)','p1(=o+v1)','p2(=o+v2)'});
-        % end
+    end
+    methods % plot graphic object
+        function pWindow(obj)
+            fill(obj.rect(1, :), obj.rect(2, :), obj.clr_faceWindow, 'FaceAlpha', 0.3, 'EdgeColor', obj.clr_edgeWindow, 'LineWidth', 1.5);
+        end
+        function pBases(obj)
+        end
+        function pLatticeWin(obj)
+        end
+        function pGridlineWin(obj)
+        end
 
     end
 end
