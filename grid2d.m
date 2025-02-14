b86f9d9c36426f4058a78424ce550e1aafe11b78
pdms_ibs_brilliantPC <vjavmvja@gmail.com>
2025-02-14 14:55:35 +0900

    redifine properties "lattice in window" and "grid in window" whenever related propertes are modified.

--- grid2d.m
+++ grid2d.m
@@ -13,7 +13,9 @@ classdef grid2d
     end
     properties (Dependent)
         augMat (3,3) table % [I]_pC^oB
-        baryBss (2,3) table        
+        baryBss (2,3) table
+        inLatt table
+        inGrid table
     end
     properties (Dependent,Hidden)
         rect (2,4) double        
@@ -33,22 +35,37 @@ classdef grid2d
             v = [0 sz(1) sz(1) 0;
                 0 0 sz(2) sz(2)];
         end
+        function v = get.inLatt(obj)
+            isnan =  any(isnan(table2array(obj.augMat)),"all");
+            if isnan
+            else
+                I_oB__pC = inv(table2array(obj.augMat)) ;
+            v =  fnd_Lattice_in_window(I_oB__pC,obj.rect);
+            end            
+        end
+        function v = get.inGrid(obj)
+            isnan =  any(isnan(table2array(obj.augMat)),"all");
+            if isnan
+            else
+                I_oB__pC = inv(table2array(obj.augMat)) ;
+                tbl_line = fnd_Grid_in_window(I_oB__pC,obj.rect);
+            end            
+        end
     end   
     methods % calculate graphic object in Window
-        function lattice_coords_in = fnd_Lattice_in_window(obj)
-            I_oB__pC = inv(table2array(obj.augMat)) ;
-            lattice_coords_in =  fnd_Lattice_in_window(I_oB__pC,obj.rect)
-        end
-        function tbl_line = fnd_Grid_in_window(obj)
-            I_oB__pC = inv(table2array(obj.augMat)) ;
-            tbl_line = fnd_Grid_in_window(I_oB__pC,obj.rect);
-        end
     end
     methods % plot graphic object
         function pWindow(obj)
             fill(obj.rect(1, :), obj.rect(2, :), obj.clr_faceWindow, 'FaceAlpha', 0.3, 'EdgeColor', obj.clr_edgeWindow, 'LineWidth', 1.5);
         end
         function pBases(obj)
+            O = obj.offset_stdpts;
+            b1 = obj.a1_stdpts;
+            b2 = obj.a2_stdpts;
+            quiver(O(1), O(2), b1(1), b1(2), 0,'Color' ,obj.clr_a1, 'LineWidth', 2, 'MaxHeadSize', 0.5); % a1 벡터
+            hold on
+            quiver(O(1), O(2), b2(1), b2(2), 0,'Color' ,obj.clr_a2, 'LineWidth', 2, 'MaxHeadSize', 0.5); % a2 벡터
+            hold off
         end
         function pLatticeWin(obj)
         end
